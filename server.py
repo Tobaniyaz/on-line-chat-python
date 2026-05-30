853ef9e581d4743f9c8431774aac249b74f2c79e
tob1k <119065763+Tobaniyaz@users.noreply.github.com>
2026-05-31 00:50:24 +0500

    Update server.py

--- server.py
+++ server.py
@@ -97,5 +97,9 @@ def disconnect():
 
 # 🚀 START (RENDER SAFE)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host="0.0.0.0", port=port)
+  socketio.run(
+    app,
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 10000)),
+    allow_unsafe_werkzeug=True
+)
