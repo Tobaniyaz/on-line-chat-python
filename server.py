3927afbb44ac50f01a7c5fadd55734de7f28573c
tob1k <119065763+Tobaniyaz@users.noreply.github.com>
2026-05-30 22:44:43 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on("message")
-def handle_message(msg):
-    send(msg, broadcast=True)
-
 @app.route("/")
 def home():
-    return "Chat server is running"
+    return render_template("index.html")
+
+@socketio.on("message")
+def handle(msg):
+    send(msg, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=10000)
